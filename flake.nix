95e919dc824b679c4919d988f8a4610c9ce37738
Dominik Schneider (schnedo) <schnedo@users.noreply.github.com>
2025-04-16 22:53:01 +0200

    add package output to flake

--- flake.nix
+++ flake.nix
@@ -6,13 +6,49 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
-      in {
+      in
+      {
+
+        packages.default =
+          let
+            cargoToml = pkgs.lib.importTOML ./Cargo.toml;
+          in
+          pkgs.rustPlatform.buildRustPackage {
+            inherit (cargoToml.package) version;
+            pname = cargoToml.package.name;
+
+            src = ./.;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+            buildInputs = with pkgs; [
+              openssl.dev
+            ];
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+            ];
+
+            meta = {
+              description = "Sync emails via imap to maildir";
+              mainProgram = cargoToml.package.name;
+              homepage = "https://github.com/schnedo/imapmaildir";
+              license = pkgs.lib.licenses.gpl3;
+              maintainers = [ "schnedo" ];
+            };
+          };
+
         devShells.default = pkgs.mkShell {
 
           packages = with pkgs; [
@@ -25,6 +61,7 @@
           # ];
 
         };
-      });
-}
 
+      }
+    );
+}
