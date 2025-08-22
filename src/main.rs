f5c2e530d8e9da1fce1061e509812b5a6d37fc6b
Dominik Schneider (schnedo) <schnedo@users.noreply.github.com>
2025-08-22 21:07:36 +0200

    add flag for nuking mails and state

--- src/main.rs
+++ src/main.rs
@@ -3,40 +3,57 @@ mod config;
 mod imap;
 mod logging;
 mod maildir;
+mod nuke;
 mod sync;
 
 use anyhow::Result;
+use clap::Parser;
 use config::Config;
 use imap::{Client, Connection};
 use maildir::MaildirRepository;
+use nuke::nuke;
 use sync::Syncer;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about=None)]
+struct Args {
+    /// `rm -rf` the configured account (WARNING: includes all mails)
+    #[arg(long)]
+    nuke: bool,
+}
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
+    let args = Args::parse();
     logging::init();
 
     let config = Config::load_from_file();
 
-    let (connection, _) = Connection::connect_to(config.host(), config.port()).await;
-    let client = Client::new(connection);
-    let mut session = client.login(config.user(), &config.password()).await?;
-    let mailbox = config
-        .mailboxes()
-        .first()
-        .expect("there should be one mailbox set");
+    if args.nuke {
+        nuke(&config);
+        Ok(())
+    } else {
+        let (connection, _) = Connection::connect_to(config.host(), config.port()).await;
+        let client = Client::new(connection);
+        let mut session = client.login(config.user(), &config.password()).await?;
+        let mailbox = config
+            .mailboxes()
+            .first()
+            .expect("there should be one mailbox set");
 
-    let uid_validity = session.select(mailbox).await?;
-    let maildir_repository = MaildirRepository::new(
-        config.account(),
-        mailbox,
-        config.maildir(),
-        config.statedir(),
-        *uid_validity,
-    );
+        let uid_validity = session.select(mailbox).await?;
+        let maildir_repository = MaildirRepository::new(
+            config.account(),
+            mailbox,
+            config.maildir(),
+            config.statedir(),
+            *uid_validity,
+        );
 
-    let mut syncer = Syncer::new(session, maildir_repository);
+        let mut syncer = Syncer::new(session, maildir_repository);
 
-    syncer.init_a_to_b().await;
+        syncer.init_a_to_b().await;
 
-    Ok(())
+        Ok(())
+    }
 }
---
+++ src/nuke.rs
@@ -0,0 +1,19 @@
+use std::fs::remove_dir_all;
+
+use log::{info, trace};
+
+use crate::config::Config;
+
+pub fn nuke(config: &Config) {
+    info!("Nuking mails and state");
+    let mails = config.maildir().join(config.account());
+    if mails.try_exists().expect("cannot read mail directory") {
+        trace!("removing {mails:?}");
+        remove_dir_all(&mails).expect("removing mails of account should succeed");
+    }
+    let state = config.statedir().join(config.account());
+    if state.try_exists().expect("cannot read state directory") {
+        trace!("removing {state:?}");
+        remove_dir_all(&state).expect("removing state of account should succeed");
+    }
+}
