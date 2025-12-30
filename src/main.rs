495ec85f4d6f9f4ce763119ac838cfee838cb597
Dominik Schneider (schnedo) <schnedo@users.noreply.github.com>
2025-12-30 16:57:23 +0100

    enable syncing multiple mailboxes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::env;
+use std::process::{Child, Command};
+
 use clap::Parser;
 mod config;
 mod imap;
@@ -21,10 +24,11 @@ struct Args {
     nuke: bool,
     #[arg(long)]
     account: String,
+    #[arg(long)]
+    mailbox: Option<String>,
 }
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<()> {
+fn main() -> Result<()> {
     let args = Args::parse();
     logging::init();
 
@@ -34,22 +38,45 @@ async fn main() -> Result<()> {
         nuke(&config);
 
         Ok(())
+    } else if let Some(mailbox) = args.mailbox {
+        let rt = tokio::runtime::Builder::new_current_thread()
+            .enable_io()
+            .build()?;
+
+        rt.block_on(async {
+            let client = Client::login(config.host(), config.port(), config.auth()).await;
+
+            let sync_handle = Syncer::sync(
+                &mailbox,
+                config.maildir_base_path(),
+                config.state_dir(),
+                client,
+            )
+            .await;
+
+            sync_handle.await
+        })?;
+
+        Ok(())
     } else {
-        let mailbox = config
+        let program = env::args_os()
+            .next()
+            .expect("first command line argument should always be program name");
+        let children: Vec<Child> = config
             .mailboxes()
-            .first()
-            .expect("there should be a mailbox configured");
+            .iter()
+            .map(|mailbox| {
+                let mut subprocess = Command::new(&program);
+                subprocess.args(["--account", &args.account, "--mailbox", mailbox]);
+                subprocess
+                    .spawn()
+                    .expect("mailbox specific subprocess should be runnable")
+            })
+            .collect();
 
-        let client = Client::login(config.host(), config.port(), config.auth()).await;
-
-        let sync_handle = Syncer::sync(
-            mailbox,
-            config.maildir_base_path(),
-            config.state_dir(),
-            client,
-        )
-        .await;
-        sync_handle.await?;
+        for mut child in children {
+            child.wait()?;
+        }
 
         Ok(())
     }
