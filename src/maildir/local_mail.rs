912d7d07cccd1faeaa1db913be38bd69f31359f4
Dominik Schneider (schnedo) <schnedo@users.noreply.github.com>
2026-02-02 10:33:54 +0100

    add tests for local_mail

--- src/maildir/local_mail.rs
+++ src/maildir/local_mail.rs
@@ -135,10 +135,15 @@ impl FromStr for LocalMailMetadata {
         let (head, flags) = s.rsplit_once(":2,").ok_or(ParseLocalMailMetadataError {
             message: "filename should contain :2,",
         })?;
-        let flags = flags
+        let flags: Result<BitFlags<Flag>, ParseLocalMailMetadataError> = flags
             .chars()
-            .map(|flag| Flag::try_from(flag).expect("char flag should be parsable"))
+            .map(|flag| {
+                Flag::try_from(flag).map_err(|_| ParseLocalMailMetadataError {
+                    message: "char flag should be parsable",
+                })
+            })
             .collect();
+        let flags = flags?;
         if let Some((fileprefix, uid)) = head.rsplit_once(",U=") {
             let uid = uid
                 .parse::<u32>()
@@ -170,13 +175,124 @@ impl From<&RemoteMailMetadata> for LocalMailMetadata {
 
 #[cfg(test)]
 mod tests {
+    use assertables::*;
+    use enumflags2::BitFlag;
+    use rstest::*;
+
+    use crate::repository::ModSeq;
+
     use super::*;
 
-    #[test]
-    fn test_maildir_flags_in_ascii_order() {
-        let flags = BitFlags::all();
-        let metadata = LocalMailMetadata::new(Uid::try_from(&3).ok(), flags, None);
+    #[fixture]
+    fn prefix() -> String {
+        "prefix".to_string()
+    }
 
+    #[fixture]
+    fn metadata(prefix: String) -> LocalMailMetadata {
+        let flags = BitFlags::all();
+        LocalMailMetadata::new(Uid::try_from(&3).ok(), flags, Some(prefix))
+    }
+
+    #[fixture]
+    fn content() -> Vec<u8> {
+        "asdfjklsadfjklsadfj".into()
+    }
+
+    #[fixture]
+    fn mail(metadata: LocalMailMetadata, content: Vec<u8>) -> LocalMail {
+        LocalMail::new(content, metadata)
+    }
+
+    #[rstest]
+    fn test_maildir_flags_in_ascii_order(metadata: LocalMailMetadata) {
         assert_eq!(metadata.string_flags(), "DFRST");
+    }
+
+    #[rstest]
+    fn test_metadata_filename_is_correct(metadata: LocalMailMetadata) {
+        let filename = metadata.filename();
+        assert_eq!("prefix,U=3:2,DFRST", filename);
+        assert_eq!(metadata, assert_ok!(filename.parse()));
+    }
+
+    #[rstest]
+    fn test_metadata_filename_without_uid_is_correct(mut metadata: LocalMailMetadata) {
+        metadata.uid = None;
+        let filename = metadata.filename();
+        assert_eq!("prefix:2,DFRST", filename);
+        assert_eq!(metadata, assert_ok!(filename.parse()));
+    }
+
+    #[rstest]
+    fn test_from_str_errors_on_invalid_filename(
+        #[values("foo", "prefix:2,s", "prefix,U=R:2,")] filename: &str,
+    ) {
+        let result = assert_err!(LocalMailMetadata::from_str(filename));
+        assert_matches!(result, ParseLocalMailMetadataError { .. });
+    }
+
+    #[rstest]
+    fn test_generate_file_prefix_is_unique() {
+        assert_ne!(
+            LocalMailMetadata::generate_file_prefix(),
+            LocalMailMetadata::generate_file_prefix()
+        );
+    }
+
+    #[rstest]
+    fn test_from_remote_mail_metadata_is_consistent() {
+        let remote =
+            RemoteMailMetadata::new(Uid::MAX, Flag::all(), assert_ok!(ModSeq::try_from(3)));
+        let result = LocalMailMetadata::from(&remote);
+        assert_eq!(remote.uid(), assert_some!(result.uid()));
+        assert_eq!(remote.flags(), result.flags());
+    }
+
+    #[rstest]
+    fn test_local_mail_unpacks(metadata: LocalMailMetadata) {
+        let content: Vec<u8> = "foo".into();
+        let expected_content = content.clone();
+        let expected_metadata = metadata.clone();
+
+        let mail = LocalMail::new(content, metadata);
+        assert_eq!(&expected_metadata, mail.metadata());
+        let (metadata, content) = mail.unpack();
+        assert_eq!(expected_metadata, metadata);
+        assert_eq!(expected_content, content);
+    }
+
+    #[rstest]
+    fn test_local_mail_debug_format_does_not_include_content(mail: LocalMail, content: Vec<u8>) {
+        assert_not_contains!(&format!("{mail:?}"), &format!("{content:?}"));
+    }
+
+    #[rstest]
+    fn test_metadata_fileprefix_getter_is_ok(metadata: LocalMailMetadata, prefix: String) {
+        assert_eq!(prefix, metadata.fileprefix());
+    }
+
+    #[rstest]
+    fn test_metadata_set_uid_is_ok(mut metadata: LocalMailMetadata) {
+        let expected = assert_ok!(Uid::try_from(4));
+        assert_ne!(Some(expected), metadata.uid());
+        metadata.set_uid(expected);
+        assert_eq!(Some(expected), metadata.uid());
+    }
+
+    #[rstest]
+    fn test_metadata_set_flags_is_ok(mut metadata: LocalMailMetadata) {
+        let expected = Flag::Seen | Flag::Deleted;
+        assert_ne!(expected, metadata.flags());
+        metadata.set_flags(expected);
+        assert_eq!(expected, metadata.flags());
+    }
+
+    #[rstest]
+    fn test_parse_local_mail_metadata_error_returns_message() {
+        let message = "aaosdfojsa";
+        let e = ParseLocalMailMetadataError { message };
+
+        assert_eq!(message, e.message());
     }
 }
