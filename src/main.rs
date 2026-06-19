cf7754a4fb262ec6d8deaaeee51c9cc2cb398486
Dominik Schneider (schnedo) <schnedo@users.noreply.github.com>
2026-06-19 23:18:01 +0200

    refactor cli

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,10 @@
 mod cli;
 
 use clap::Parser;
-use log::LevelFilter;
 
-use imapmaildir::{config::Account, logging};
-
-#[derive(Parser, Debug)]
-#[command(version, about, long_about=None)]
-struct Args {
-    /// `rm -rf` the configured account (WARNING: includes all mails)
-    #[arg(long)]
-    nuke: bool,
-    #[arg(long)]
-    account: String,
-    #[arg(long)]
-    mailbox: Option<String>,
-    #[arg(long, default_value_t=LevelFilter::Trace)]
-    log_level: LevelFilter,
-}
+use crate::cli::Cli;
 
 fn main() {
-    let args = Args::parse();
-    logging::init(args.log_level);
-    let config = Account::load_from_file(&args.account);
-
-    cli::run(&args, config);
+    let cli = Cli::parse();
+    cli::run(&cli);
 }
